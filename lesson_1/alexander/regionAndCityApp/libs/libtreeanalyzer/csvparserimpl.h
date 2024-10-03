ce7407dc2d62fcff80881f754756107a2ad7a1cf
alexandershataev <alexandershataev@mail.ru>
2024-10-04 00:44:53 +0300

    немного увеличил производительности за счет boost containers

--- lesson_1/alexander/regionAndCityApp/libs/libtreeanalyzer/csvparserimpl.h
+++ lesson_1/alexander/regionAndCityApp/libs/libtreeanalyzer/csvparserimpl.h
@@ -5,12 +5,12 @@
 #ifndef CSVPARSERIMPL_H
 #define CSVPARSERIMPL_H
 
-#include <boost/container/flat_map.hpp>
-#include <boost/fusion/adapted/std_pair.hpp>
 #include <boost/spirit/home/x3.hpp>
 #include <boost/spirit/include/qi.hpp>
 
-#include <map>
+#include <boost/container/static_vector.hpp>
+#include <boost/unordered/unordered_map.hpp>
+#include <boost/container/list.hpp>
 
 #include "treeanalyzer/regions.h"
 
@@ -18,10 +18,12 @@ namespace qi = boost::spirit::qi;
 
 namespace TRA::CSVParserImpl {
 
+    constexpr static int COLUMN_COUNT = 11;
+
     using Column = std::string;
-    using Columns = std::vector<Column>;
+    using Columns = boost::container::static_vector<Column, COLUMN_COUNT>;
     using Record = Columns;
-    using Table = std::vector<Record>;
+    using Table = boost::container::list<Record>;
 
     template<typename It>
     struct CsvGrammarRule : qi::grammar<It, Table(), qi::blank_type> {
@@ -35,7 +37,7 @@ namespace TRA::CSVParserImpl {
             column = quoted | *~char_(colsep);
             quoted = '"' >> *("\"\"" | ~char_('"')) >> '"';
 
-            BOOST_SPIRIT_DEBUG_NODES((start)(line)(column)(quoted));
+            //BOOST_SPIRIT_DEBUG_NODES((start)(line)(column)(quoted));
         }
 
     private:
@@ -47,24 +49,28 @@ namespace TRA::CSVParserImpl {
 
     template<typename It>
     static std::set<RegionNodePtr<StateRegion> > parseFromCsv(It begin, It end) {
-        std::map<std::string, RegionNodePtr<StateRegion> > states;
+        boost::unordered_map<std::string, RegionNodePtr<StateRegion> > states;
 
         CsvGrammarRule<It> p;
         Table parsed;
 
         if (bool ok = qi::phrase_parse(begin, end, p, qi::blank, parsed)) {
             for (auto &line: parsed) {
-                auto &&cityName = std::move(line[0]);
-                auto &&stateName = std::move(line[4]);
+                if (line.size() != COLUMN_COUNT) {
+                    //TO-DO: handle error
+                    continue;
+                }
+                auto &&cityName = line[0];
+                auto &&stateName = line[4];
                 auto itState = states.find(stateName);
                 RegionNodePtr<StateRegion> curState;
                 if (itState != states.end()) {
                     curState = itState->second;
                 } else {
-                    curState = std::make_shared<TreeNodeRegion<StateRegion> >(StateRegion(stateName));
+                    curState = std::make_shared<TreeNodeRegion<StateRegion> >(StateRegion(std::string(stateName)));
                     states.insert(std::make_pair(stateName, curState));
                 }
-                curState->addChild(std::make_shared<TreeNodeRegion<CityRegion> >(CityRegion(cityName)));
+                curState->addChild(std::make_shared<TreeNodeRegion<CityRegion>>(CityRegion(cityName)));
             }
         } else {
             return {};
